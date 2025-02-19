1c1759e325171e90e07dc66e69ec4ea60e8d3422
Setareh <22ss75@queensu.ca>
2025-02-19 15:59:43 -0500

    Added the pipemiddle code

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -8,10 +8,10 @@ public class Logic : MonoBehaviour
     public Text scoreText;
     public int playerScore = 0;
 
-    [ContextMenu("Increase Score")]
     public void addScore()
     {
         playerScore = playerScore + 1;
         scoreText.text = playerScore.ToString();
+        Debug.Log("Score added");
     }
 }
---
+++ Assets/Scripts/PipeMiddle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMiddle : MonoBehaviour
+{
+    public Logic logic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("LogicTag").GetComponent<Logic>();
+        if (logic != null)
+        {
+            // GameObject was found and Logic component is assigned
+            Debug.Log("Logic object found!");
+        }
+        else
+        {
+            // GameObject was not found
+            Debug.Log("Logic object not found.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision");
+        logic.addScore();
+    }
+}
