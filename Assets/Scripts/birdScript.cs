826a81d05299e15b6b99fa254a37dbc31ef1198c
Setareh <22ss75@queensu.ca>
2025-02-19 13:58:58 -0500

    Added the Bird to the scene, this bird will go up with clicking  the space bottom

--- Assets/Scripts/birdScript.cs
+++ Assets/Scripts/birdScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class birdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidBody;
+    public float flapStrength;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.velocity = Vector2.up * flapStrength;
+        }
     }
 }
